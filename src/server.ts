a1f548d7f89e8bd50e82b2f9770e741cfdb17b7c
Abhishek Karthik Manikandan <abhishekkarthikmanikandan@Abhisheks-MacBook-Pro.local>
2026-02-08 21:42:34 -0800

    Added eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-console.log('Welcome to full stack development with Node.js and Express!')
+// console.log('Welcome to full stack development with Node.js and Express!')
 
 function welcome(name: string) {
-    console.log(`Welcome, ${name}!`)
+    // console.log(`Welcome, ${name}!`)
+    return `Welcome, ${name}!`
 }
 
 welcome('Abhishek Karthik')
