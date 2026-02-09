c4e7f7df78a54ddaf5106ffaeae5478f68bc50cc
Abhishek Karthik Manikandan <abhishekkarthikmanikandan@Abhisheks-MacBook-Pro.local>
2026-02-08 21:30:25 -0800

    Added typescript and prettier formatter

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+console.log('Welcome to full stack development with Node.js and Express!')
+
+function welcome(name: string) {
+    console.log(`Welcome, ${name}!`)
+}
+
+welcome('Abhishek Karthik')
